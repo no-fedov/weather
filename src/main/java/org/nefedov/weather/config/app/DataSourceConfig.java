b81c5408045412e3ec05f0e17528fe339d530bf4
no-fedov <nefedov.evgeniy.d@yandex.com>
2026-02-19 19:09:26 +0300

    refactor: combine beans which depend on the datasource into one class

--- src/main/java/org/nefedov/weather/config/app/DataSourceConfig.java
+++ src/main/java/org/nefedov/weather/config/app/DataSourceConfig.java
@@ -2,10 +2,17 @@ package org.nefedov.weather.config.app;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import liquibase.integration.spring.SpringLiquibase;
+import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.orm.jpa.hibernate.HibernateTransactionManager;
+import org.springframework.orm.jpa.hibernate.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 public class DataSourceConfig {
@@ -15,5 +22,33 @@ public class DataSourceConfig {
         Class.forName("org.mariadb.jdbc.Driver");
         HikariConfig hikariConfig = new HikariConfig("hikari.properties");
         return new HikariDataSource(hikariConfig);
+    }
+
+    @Bean
+    @DependsOn("liquibase")
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setPackagesToScan("org.nefedov.weather.application.persistence.entity");
+        Properties props = new Properties();
+        props.setProperty("hibernate.show_sql", "true");
+        props.setProperty("hibernate.format_sql", "true");
+        props.setProperty("hibernate.hbm2ddl.auto", "validate");
+        sessionFactory.setHibernateProperties(props);
+        return sessionFactory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(SessionFactory sessionFactory) {
+        return new HibernateTransactionManager(sessionFactory);
+    }
+
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:db/changelog/db.changelog-master.xml");
+        liquibase.setDataSource(dataSource);
+        return liquibase;
     }
 }
