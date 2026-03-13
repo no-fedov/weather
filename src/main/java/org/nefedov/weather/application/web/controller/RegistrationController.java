d2d6c832d5c7ca4acb45c837811d6ffdcc59ad2a
surhay <nefedov.nefedov.d@yandex.ru>
2026-03-13 11:15:37 +0300

    fix: сделал елки палки с редиректом при регистрации пользователя, не знаю на сколько это корректно нужно ревью опытного разраба

--- src/main/java/org/nefedov/weather/application/web/controller/RegistrationController.java
+++ src/main/java/org/nefedov/weather/application/web/controller/RegistrationController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.time.LocalDateTime;
@@ -31,13 +32,13 @@ public class RegistrationController {
     }
 
     @PostMapping
-//    @ResponseStatus(HttpStatus.CREATED)
-    public ModelAndView registration(@Valid UserCreateDto dto, HttpServletResponse response) {
+    public String registration(@Valid UserCreateDto dto,
+                                     HttpServletResponse response,
+                                     RedirectAttributes redirectAttributes) {
         SessionDto registration = registrationService.registration(dto, LocalDateTime.now());
         Cookie cookie = new Cookie("session-id", registration.uuid().toString());
         response.addCookie(cookie);
-        RedirectView redirectView = new RedirectView("/home", true);
-        redirectView.setStatusCode(HttpStatus.CREATED);
-        return new ModelAndView(redirectView);
+        redirectAttributes.addAttribute("new_user", dto.getLogin());
+        return "redirect:/home";
     }
 }
