729f0d884af837ed1f8c671bfc03de2665eab5e3
surhay <nefedov.nefedov.d@yandex.ru>
2026-03-13 11:15:37 +0300

    fix: сделал елки палки с редиректом при регистрации пользователя, не знаю на сколько это корректно нужно ревью опытного разраба

--- src/main/java/org/nefedov/weather/application/web/controller/HomeController.java
+++ src/main/java/org/nefedov/weather/application/web/controller/HomeController.java
@@ -1,18 +1,23 @@
 package org.nefedov.weather.application.web.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.nefedov.weather.application.dto.LocationUserResponseDto;
 import org.nefedov.weather.application.dto.SessionDto;
 import org.nefedov.weather.application.dto.WeatherResponseDto;
 import org.nefedov.weather.application.service.WeatherService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/home")
@@ -22,7 +27,11 @@ public class HomeController {
     private final WeatherService weatherService;
 
     @GetMapping
-    public String home(Model model, @RequestAttribute("session") SessionDto session) {
+    public String home(Model model,
+                       @RequestAttribute("session") SessionDto session,
+                       @RequestParam("new_user") Optional<String> newUser,
+                       HttpServletResponse response) {
+        newUser.ifPresent(param -> response.setStatus(HttpStatus.CREATED.value()));
         List<WeatherResponseDto> weathers = weatherService.findForUser(session.userId());
         model.addAttribute("weathers", weathers);
         return "home";
