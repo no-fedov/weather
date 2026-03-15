bdfa73c4bb1941111d50cbd26c4c7bbaf2dba221
surhay <nefedov.nefedov.d@yandex.ru>
2026-03-15 14:53:33 +0300

    ...

---
+++ src/main/java/org/nefedov/weather/application/exception/auth/SessionExpiredException.java
@@ -0,0 +1,7 @@
+package org.nefedov.weather.application.exception.auth;
+
+import lombok.experimental.StandardException;
+
+@StandardException
+public class SessionExpiredException extends AuthException {
+}
