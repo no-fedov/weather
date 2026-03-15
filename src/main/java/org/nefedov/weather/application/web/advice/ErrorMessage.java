f5729890c282356bb001b3afcdd2858e70389907
no-fedov <nefedov.evgeniy.d@yandex.com>
2026-03-15 19:43:57 +0300

    fix: delete redirect from validation handler, return template in time

--- src/main/java/org/nefedov/weather/application/web/advice/ErrorMessage.java
+++
@@ -1,8 +0,0 @@
-package org.nefedov.weather.application.web.advice;
-
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-public class ErrorMessage {
-    private final String message;
-}
