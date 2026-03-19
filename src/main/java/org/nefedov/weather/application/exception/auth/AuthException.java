9f506c13aef2dfde8a98ad2177296a9f25a22e35
surhay <nefedov.nefedov.d@yandex.ru>
2026-03-19 12:24:41 +0300

    refactor: заменил auth фильтр на interceptor

--- src/main/java/org/nefedov/weather/application/exception/auth/AuthException.java
+++ src/main/java/org/nefedov/weather/application/exception/auth/AuthException.java
@@ -6,13 +6,19 @@ import lombok.experimental.StandardException;
 
 @StandardException
 public class AuthException extends RuntimeException {
+
+    public AuthException() {
+        super(AuthExceptionMessages.NOT_AUTHORIZED.message);
+    }
+
     @Getter
     @RequiredArgsConstructor
     protected enum AuthExceptionMessages {
 
         LOGIN_ALREADY_EXISTS("Account with this login already exists!"),
         SESSION_EXPIRATION("Please sign-in again!"),
-        LOGIN_OR_PASSWORD("Invalid username or password.");
+        LOGIN_OR_PASSWORD("Invalid username or password."),
+        NOT_AUTHORIZED("Please authorize!");
 
         private final String message;
     }
--- src/main/java/org/nefedov/weather/application/web/interceptor/AuthInterceptor.java
+++ src/main/java/org/nefedov/weather/application/web/interceptor/AuthInterceptor.java
@@ -1,7 +1,5 @@
-package org.nefedov.weather.application.web.filter;
+package org.nefedov.weather.application.web.interceptor;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,43 +9,35 @@ import org.nefedov.weather.application.exception.auth.AuthException;
 import org.nefedov.weather.application.exception.auth.SessionExpiredException;
 import org.nefedov.weather.application.service.SessionManager;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerInterceptor;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.UUID;
 
-@Component("authFilter")
+@Component
 @RequiredArgsConstructor
-public class AuthFilter extends OncePerRequestFilter {
+public class AuthInterceptor implements HandlerInterceptor {
 
     private final SessionManager sessionManager;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        Cookie sessionCookie = Arrays.stream(request.getCookies())
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Cookie[] sessionCookies = request.getCookies();
+        if (Objects.isNull(sessionCookies)) {
+            throw new AuthException();
+        }
+        Cookie authCookie = Arrays.stream(sessionCookies)
                 .filter(cookie -> Objects.equals(cookie.getName(), "session-id"))
                 .findFirst()
                 .orElseThrow(AuthException::new);
-        String sessionId = sessionCookie.getValue();
+        String sessionId = authCookie.getValue();
         UUID uuid = UUID.fromString(sessionId);
         SessionDto session = sessionManager.find(uuid);
         validateSession(session);
         request.setAttribute("session", session);
-        filterChain.doFilter(request, response);
-    }
-
-    @Override
-    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-        String servletPath = request.getServletPath();
-        if (servletPath.equals("/") || servletPath.equals("/sign-in") || servletPath.equals("/sign-up")) {
-            return true;
-        }
-        return servletPath.startsWith("/css")
-                || servletPath.startsWith("/js")
-                || servletPath.startsWith("/error");
+        return true;
     }
 
     private void validateSession(SessionDto session) {
