8affded547cffed0540bf429e387b6fc2592bcd9
no-fedov <145092682+no-fedov@users.noreply.github.com>
2026-03-20 16:39:20 +0300

    Revert "refactor: add session expiration to properties, register duration formatter"
    This reverts commit e95951c01f8556478c7ba48cd52d48c0b31ca055.

--- src/main/java/org/nefedov/weather/application/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.nefedov.weather.application.exception;
-
-import lombok.experimental.StandardException;
-
-@StandardException
-public class UserNotFoundException extends NotFoundException {
-}
