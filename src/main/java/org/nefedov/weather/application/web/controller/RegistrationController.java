151a309496a0da2172ff722091ed4e0b48d8c86a
no-fedov <145092682+no-fedov@users.noreply.github.com>
2026-03-20 16:39:20 +0300

    Revert "fix: сделал елки палки с редиректом при регистрации пользователя, не знаю на сколько это корректно нужно ревью опытного разраба"
    This reverts commit d2d6c832d5c7ca4acb45c837811d6ffdcc59ad2a.

--- src/main/java/org/nefedov/weather/application/web/controller/RegistrationController.java
+++ src/main/java/org/nefedov/weather/application/web/controller/RegistrationController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.time.LocalDateTime;
@@ -32,13 +31,13 @@ public class RegistrationController {
     }
 
     @PostMapping
-    public String registration(@Valid UserCreateDto dto,
-                                     HttpServletResponse response,
-                                     RedirectAttributes redirectAttributes) {
+//    @ResponseStatus(HttpStatus.CREATED)
+    public ModelAndView registration(@Valid UserCreateDto dto, HttpServletResponse response) {
         SessionDto registration = registrationService.registration(dto, LocalDateTime.now());
         Cookie cookie = new Cookie("session-id", registration.uuid().toString());
         response.addCookie(cookie);
-        redirectAttributes.addAttribute("new_user", dto.getLogin());
-        return "redirect:/home";
+        RedirectView redirectView = new RedirectView("/home", true);
+        redirectView.setStatusCode(HttpStatus.CREATED);
+        return new ModelAndView(redirectView);
     }
 }
