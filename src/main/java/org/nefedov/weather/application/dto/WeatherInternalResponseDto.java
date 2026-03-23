9cc398a1a35fe49abcdcceb10cf2f0b6f5c4cc36
no-fedov <nefedov.evgeniy.d@yandex.ru>
2026-03-23 23:02:40 +0300

    fix: home render

--- src/main/java/org/nefedov/weather/application/dto/WeatherInternalResponseDto.java
+++ src/main/java/org/nefedov/weather/application/dto/WeatherInternalResponseDto.java
@@ -8,6 +8,6 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class WeatherInternalResponseDto {
-    private WeatherExternalResponseDto externalResponse;
+    private WeatherExternalResponseDto weather;
     private Integer userLocationId;
 }
